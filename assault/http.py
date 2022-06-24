f67f2135c135911c1f1a56769a7b5af9ada69b11
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-24 21:12:27 +0100

    http file for async and await

--- assault/http.py
+++ assault/http.py
@@ -1,4 +1,6 @@
 import asyncio
+from asyncio import tasks
+import queue
 
 
 def fetch():
@@ -14,11 +16,33 @@ def worker():
     pass
 
 
-def distribute_work():
+async def distribute_work(url, requests, concurrency, results):
     """Divide up the work into batches and collect the final results"""
-    pass
+    queue = asyncio.Queue()
+
+    for _ in range(requests):
+        queue.put_nowait(url)
+
+    tasks = []
+    for i in range(concurrency):
+        tasks = asyncio.create_task(worker(f"worker-{i+1}", queue, results))
+        tasks.append(task)
+
+    started_at = time.monotonic()
+    await queue.join()
+    total_time = time.monotonic() - started_at
+
+    for task in tasks:
+        task.cancel()
+
+    print("---")
+    print(
+        f"{concurrency} workers took {total_time:.2f} seconds to complete {len(results)} requests"
+    )
 
 
-def assault():
+def assault(url, requests, concurrency):
     """Entrypoint to making"""
-    pass
+    results = []
+    asyncio.run(distribute_work(url, requests, concurrency, results))
+    print(results)
