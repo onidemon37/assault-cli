de95cd20fdb4840d87f3e8f5183fcbd254e1c32f
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-24 15:18:38 +0100

    http file for async and await

---
+++ assault/http.py
@@ -0,0 +1,24 @@
+import asyncio
+
+
+def fetch():
+    """Make the request and return the results"""
+    pass
+
+
+def worker():
+    """A function to take unmake requests from a
+    queu and perform the work then add results
+    to the results list.
+    """
+    pass
+
+
+def distribute_work():
+    """Divide up the work into batches and collect the final results"""
+    pass
+
+
+def assault():
+    """Entrypoint to making"""
+    pass
