1904b0162647bafc0f7ac3340e9e2fe31c55aa08
Edino Tavares Moniz <41202410+onidemon37@users.noreply.github.com>
2022-06-26 19:33:38 +0100

    doctest, to leverage automated tests (#6)
    * doctest, to leverage automated tests
    
    * doctest, completion, and usage in readme
    
    * doctest, completion, and usage in readme

--- assault/cli.py
+++ assault/cli.py
@@ -1,7 +1,10 @@
 import click
+import sys
+import json
+from typing import TextIO
 
 from .http import assault
-
+from .stats import Results
 
 @click.command()
 @click.option("--requests", "-r", default=500, help="Number of requests")
@@ -9,12 +12,21 @@ from .http import assault
 @click.option("--json-file", "-j", default=None, help="Path to output JSON file")
 @click.argument("url")
 def cli(requests, concurrency, json_file, url):
-    print(f"Requests: {requests}")
-    print(f"Concurrency: {concurrency}")
-    print(f"Json File: {json_file}")
-    print(f"URL: {url}")
-    assault(url, requests, concurrency)
-
-
-if __name__ == "__main__":
-    cli()
+    total_time, request_dicts = assault(url, requests, concurrency)
+    results = Results(total_time, request_dicts)
+    display(results, json_file)
+    
+def display(results, json_file):
+    if json_file:
+        # write to json file
+        print("We're writing to a JSON File")
+    else:
+        # Print screen
+        print(".... Done!")
+        print("--- Results ---")
+        print(f"Successful requests\t{ results.successful_requests()}")
+        print(f"Slowest            \t{results.slowest()}")
+        print(f"Fastest            \t{results.fastest()}")
+        print(f"Total Time         \t{results.total_time}")
+        print(f"Requests Per Minute\t{results.requests_per_minute()}")
+        print(f"Requests Per Second\t{results.requests_per_second()}")
--- assault/http.py
+++ assault/http.py
@@ -45,13 +45,11 @@ async def distribute_work( url, requests, concurrency, results):
     for task in tasks:
         task.cancel()
 
-    print("---")
-    print(
-        f"{concurrency} workers took {total_time:.2f} seconds to complete {len(results)} requests"
-    )
+    return total_time
+
 
 # Entrypoint to making requests
 def assault(url, requests, concurrency):
     results = []
-    asyncio.run(distribute_work(url, requests, concurrency, results))
-    print(results)
+    total_time = asyncio.run(distribute_work(url, requests, concurrency, results))
+    return (total_time, results)
